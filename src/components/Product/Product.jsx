435433329fff442038f7f5cf78c69b3f7f7a79ba
jinan <muhamadmiftahul.27@gmail.com>
2025-02-08 07:06:02 +0700

    api card media image

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,7 +6,7 @@ const Product = ({ product, onEdit, onDelete }) => (
     <CardMedia
       component="img"
       height="140"
-      image={`https://be-toko-hp-production.up.railway.app${product.pd_image}`}
+      image={`https://backend-marketplace-production.up.railway.app${product.pd_image}`}
       alt={product.pd_name}
     />
     <CardContent>
